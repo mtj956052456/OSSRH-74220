7bb61259b00872f61bf8cd73ef9d09ff77d5f5df
956052456@qq.com <mtj10097611>
2021-10-19 09:25:46 +0800

    'init'

---
+++ app/src/main/java/com/example/mtjapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mtjapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val sss = "孟先生"
+        val sss1 = "孟先生"
+        val sss2 = "孟先生"
+    }
+}
