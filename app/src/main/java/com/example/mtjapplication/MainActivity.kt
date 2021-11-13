9ee50a5c477fdc00f4be8b2a7d5bc65f3da7625d
956052456@qq.com <mtj10097611>
2021-11-13 16:12:47 +0800

    测试1.0.4maven发布成功

--- app/src/main/java/com/example/mtjapplication/MainActivity.kt
+++ app/src/main/java/com/example/mtjapplication/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.mtjapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mtj.common.util.Lg
+import com.mtj.common.util.Lg.lgd
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Lg.lgd("发布maven成功")
+        lgd("发布maven成功")
     }
 }
